e6916bd14c982af6b8149a0584b40f16f165e7f5
Jesus Arciniega <dev.jesusarc@gmail.com>
2025-03-24 18:46:17 -0600

    Services

--- src/app/page.js
+++ src/app/page.js
@@ -52,7 +52,9 @@ export default function Home() {
           />
         </div>
         <div className={styles.videoContainer}>
-          <h2 className={styles.videoTitle}>Semblanza de la Dra. <br /> Mileidy Fernández Ribot</h2>
+          <h2 className={styles.videoTitle}>
+            Semblanza de la Dra. <br /> Mileidy Fernández Ribot
+          </h2>
           <div className={styles.videoWrapper}>
             <iframe
               className={styles.video}
@@ -63,6 +65,51 @@ export default function Home() {
               width="100%"
               height="400px"
             ></iframe>
+          </div>
+        </div>
+        <div className={styles.services}>
+          <h2>Nuestros Servicios</h2>
+          <p className={styles.servicesIntro}>
+            Descubre los tratamientos premium de rejuvenecimiento que tenemos
+            para ti.
+          </p>
+
+          <div className={styles.servicesList}>
+            <div className={styles.serviceImage}>
+              <Image
+                src="https://dramileidyfdzribot.com/wp-content/uploads/2024/08/servicios-inicio-1.jpg"
+                alt="Servicios de medicina estética"
+                width={300}
+                height={380}
+              />
+            </div>
+
+            <div className={styles.servicesContent}>
+              <div className={styles.serviceItem}>
+                <div className={styles.serviceDiamond}>
+                  <Diamond />
+                </div>
+                <h3>Medicina Estética</h3>
+                <p>
+                  Procedimientos avalados por Médicos con más de 15 años de
+                  experiencia, que dignifican la práctica de la Medicina
+                  Estética alineando el Ser al Parecer.
+                </p>
+                <button className={styles.serviceButton}>Conocer más</button>
+              </div>
+
+              <div className={styles.serviceItem}>
+                <div className={styles.serviceDiamond}>
+                  <Diamond />
+                </div>
+                <h3>Dermatológica Campestre</h3>
+                <p>
+                  Soluciones integrales de productos top en el mercado para el
+                  manejo, cuidado y protección de la piel.
+                </p>
+                <button className={styles.serviceButton}>Conocer más</button>
+              </div>
+            </div>
           </div>
         </div>
       </main>
