272722a376e5b660ad32c32e8cb1e2af64f36556
Jesus Arciniega <dev.jesusarc@gmail.com>
2025-03-29 01:13:04 -0600

    Trayectoria mobile

--- src/app/trayectoria/page.js
+++ src/app/trayectoria/page.js
@@ -11,6 +11,8 @@ import Chapters from "../../components/Chapters";
 export default function Trayectoria() {
   const [activeTab, setActiveTab] = useState("Trayectoria");
   const [activeTitle, setActiveTitle] = useState("Trayectoria");
+  const [activeImage, setActiveImage] = useState("/dra_path.jpg");
+
   const slides = [
     {
       id: 1,
@@ -93,21 +95,39 @@ Tiene mucho que ver con nuestro autoconcepto, autoestima y amor propio.`,
     <div>
       <Title_Banner title={activeTitle} />
       <div className={styles.tabsContainer}>
-        <div className={styles.tabsHeader}>
-          {tabsData.map((tab) => (
-            <button
-              key={tab.id}
-              className={`${styles.tabButton} ${
-                activeTab === tab.id ? styles.activeTab : ""
-              }`}
-              onClick={() => {
-                setActiveTab(tab.id);
-                setActiveTitle(tab.title);
-              }}
-            >
-              {tab.title} <span className={styles.arrow}>→</span>
-            </button>
-          ))}
+        <div className={styles.heroSection}>
+          <div className={styles.heroImage}>
+            <img src={activeImage} alt="Dra. Mileidy" />
+          </div>
+          <div className={styles.heroContent}>
+            <h1 className={styles.mainTitle}>
+              El crecimiento de una mujer visionaria.
+            </h1>
+            <div className={styles.tabsContainer}>
+              <div className={styles.tabsHeader}>
+                {tabsData.map((tab) => (
+                  <button
+                    key={tab.id}
+                    className={`${styles.tabButton} ${
+                      activeTab === tab.id ? styles.activeTab : ""
+                    }`}
+                    onClick={() => {
+                      setActiveTab(tab.id);
+                      setActiveTitle(tab.title);
+                      setActiveImage(tab.image);
+                    }}
+                  >
+                    <div className={styles.tabItemContent}>
+                      <div className={styles.arrowCircle}>
+                        <span className={styles.arrow}>→</span>
+                      </div>
+                      <span className={styles.tabTitle}>{tab.title}</span>
+                    </div>
+                  </button>
+                ))}
+              </div>
+            </div>
+          </div>
         </div>
 
         <div className={styles.tabContent}>
@@ -115,15 +135,7 @@ Tiene mucho que ver con nuestro autoconcepto, autoestima y amor propio.`,
             (tab) =>
               activeTab === tab.id && (
                 <div key={tab.id} className={styles.contentWrapper}>
-                  <div className={styles.imageContainer}>
-                    <img
-                      src={tab.image}
-                      alt={tab.title}
-                      className={styles.tabImage}
-                    />
-                  </div>
                   <div className={styles.textContainer}>
-                    <h2 className={styles.contentHeading}>{tab.heading}</h2>
                     <p className={styles.contentDescription}>
                       {tab.description}
                     </p>
@@ -185,13 +197,13 @@ Tiene mucho que ver con nuestro autoconcepto, autoestima y amor propio.`,
 
           {activeTab === "MedicoEstetico" && (
             <div className={styles.medicalAdditionalContent}>
-              <TextImage 
+              <TextImage
                 textContent={[
                   "Es miembro fundador y directora general de la empresa clínica GEN conformada por la Clínica Imagen Art y Farmacia Dermatológica Campestre.",
                   "Ha sido acreedora de dos diplomados en el Tecnológico de Monterrey, Dirección Gerencial y Liderazgo.",
                   "Cuenta además con una certificación internacional en liderazgo por Johnson Maxwell, también certificada en el arte de hablar en público por el doctor César Lozano.",
                   "Finalizó un programa de capacitación en Monterrey titulado Empresarios de alto rendimiento.",
-                  "Actualmente miembro activo de Board Media una comunidad colaborativa de CEO'S de habla hispana."
+                  "Actualmente miembro activo de Board Media una comunidad colaborativa de CEO'S de habla hispana.",
                 ]}
                 imageSrc="/medical.png"
                 imageAlt="Doctora trabajando"
@@ -206,7 +218,7 @@ Tiene mucho que ver con nuestro autoconcepto, autoestima y amor propio.`,
 
           {activeTab === "Empresaria" && (
             <div className={styles.businessAdditionalContent}>
-              <TextImage 
+              <TextImage
                 textContent={[
                   "Disfruta apoyar y participar activamente en las asociaciones civiles: Mujer fascinante y LA ROCA CC, agradecida con México que dice tener tatuado en su corazón.",
                   "Elige aceptar el reto de ser la presidenta fundadora de una de las asociaciones más influyentes de México en cuanto a representación de mujeres empresarias, la Asociación Mexicana de Mujeres Jefas de Empresa Representación Tijuana, por sus siglas AMMJE.",
@@ -214,7 +226,7 @@ Tiene mucho que ver con nuestro autoconcepto, autoestima y amor propio.`,
                   "Su desempeño y compromiso dentro de la asociación en muy poco tiempo la llevaron a ser nombrada, el 19 de enero de 2024, Delegada de la Región Norte.",
                   "Con la intención de continuar fortaleciendo AMMJE Representación Tijuana y a su vez apoyando en aperturar representaciones en Baja California acepta el reto de nuevamente ser elegida presidenta de representación.",
                   "Su intención es aprender y aprender enseñando porque reconoce que todas podemos ser mentoras de alguien más.",
-                  "Su visión personal: Dignificar la práctica de la medicina estética en tanto apoya en la inclusión exitosa de la mujer en el mundo empresarial."
+                  "Su visión personal: Dignificar la práctica de la medicina estética en tanto apoya en la inclusión exitosa de la mujer en el mundo empresarial.",
                 ]}
                 imageSrc="/empresaria.jpg"
                 imageAlt="Dra. Mileidy como empresaria"
@@ -222,43 +234,56 @@ Tiene mucho que ver con nuestro autoconcepto, autoestima y amor propio.`,
 
               <h3>Reconocimientos</h3>
               <p>
-                Acreedora de la Distinción Mujer líder en Baja California y reconocida por su labor empresarial en diversos medios.
+                Acreedora de la Distinción Mujer líder en Baja California y
+                reconocida por su labor empresarial en diversos medios.
               </p>
             </div>
           )}
 
           {activeTab === "Escritora" && (
             <div className={styles.writterAdditionalContent}>
-              <TextImage 
+              <TextImage
                 textContent={[
                   `Todas atravesamos desiertos, vicisitudes, sin embargo, a pesar de todos nuestros desaciertos, "hoy es una nueva oportunidad de hacerlo diferente".`,
                   "Y no se trata de, ¿que pase para llegar aquí?, sino de agradecer que estoy aquí. El libro es un viaje o una invitación para construirte.",
-                  "Y este viaje lo harás por medio de sus 5 capítulos:"
+                  "Y este viaje lo harás por medio de sus 5 capítulos:",
                 ]}
                 imageSrc="/escritora.jpg"
                 imageAlt="Dra. Mileidy como escritora"
               />
               <Chapters />
-              
+
               <div className={styles.bookConclusion}>
-                <p>Finalmente, si no estás cómoda con algo de tu cuerpo, tu alma o tu espíritu, te quiero decir:</p>
+                <p>
+                  Finalmente, si no estás cómoda con algo de tu cuerpo, tu alma
+                  o tu espíritu, te quiero decir:
+                </p>
                 <blockquote className={styles.quote}>
-                  No te detengas en los valles, porque ese no es el destino
-                  que Dios tiene preparado para tu vida.
+                  No te detengas en los valles, porque ese no es el destino que
+                  Dios tiene preparado para tu vida.
                 </blockquote>
               </div>
-              
+
               <div className={styles.bookHighlight}>
                 <div className={styles.bookImageContainer}>
-                  <img src="/dra_path_esc.jpg" alt="Dra. Mileidy como escritora" />
+                  <img
+                    src="/dra_path_esc.jpg"
+                    alt="Dra. Mileidy como escritora"
+                  />
                 </div>
                 <div className={styles.bookMessage}>
                   <h3>El Arte de Pulir un Diamante® nos recuerda:</h3>
                   <p className={styles.bookQuote}>
-                    Nuestra piel es el traje a la medida que nos acompañará a lo largo de toda la vida, poner especial énfasis en ella nos permitirá enviar un mensaje anónimo al mundo, será una acción que nos apoyará a brillar como el diamante que siempre hemos sido.
+                    Nuestra piel es el traje a la medida que nos acompañará a lo
+                    largo de toda la vida, poner especial énfasis en ella nos
+                    permitirá enviar un mensaje anónimo al mundo, será una
+                    acción que nos apoyará a brillar como el diamante que
+                    siempre hemos sido.
                   </p>
                   <p className={styles.bookQuote}>
-                    La obsesión consigo mismo es un callejón sin salida. La atención en los demás nos orienta a una vida libre y espaciosa".
+                    La obsesión consigo mismo es un callejón sin salida. La
+                    atención en los demás nos orienta a una vida libre y
+                    espaciosa".
                   </p>
                   <div className={styles.diamondIcon}>
                     <Diamond />
