d9cbcd1c968b1ac09251ca3fa27e874732872341
Jesus Arciniega <dev.jesusarc@gmail.com>
2025-04-10 15:05:01 -0600

    Search removed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -81,22 +81,6 @@ const Header = () => {
               {item.label}
             </Link>
           ))}
-          <button aria-label="Search" className={styles.searchButton}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className={styles.searchIcon}
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-            </svg>
-          </button>
         </nav>
 
         {/* Mobile Menu Button */}
